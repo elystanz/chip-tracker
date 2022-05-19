3c8e119c2c10c6ef93d5d22d0ef735f0ec80af07
HoneyTwix <chocolateenchanted@gmail.com>
2022-05-18 20:26:02 -0500

    were frustrated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const PORT = process.env.NODE_ENV || 3001;
 
 const app = express()
+app.get('/',(req, res)=>{
+    res.sendStatus(200)
+})
 app.listen(PORT,() => {
     console.log(`listening on http://localhost:${PORT}`)
 })
