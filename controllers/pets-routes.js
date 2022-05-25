de0891af7d16c56655e110bf73b83a09ced720a6
elystanz <elysestanziale@gmail.com>
2022-05-25 16:07:15 -0500

    improve routes, fix CSS

--- controllers/pets-routes.js
+++ controllers/pets-routes.js
@@ -27,6 +27,30 @@ router.get('/', async (req, res) => {
     })
 })
 
+router.get('/vet-login', (req, res) => {
+    res.render('vet-login', {
+        pets
+    })
+})
+
+router.get('/vet', (req, res) => {
+    res.render('vet', {
+        pets
+    })
+})
+
+router.get('/client-login', (req, res) => {
+    res.render('client-login', {
+        pets
+    })
+})
+
+router.get('/client', (req, res) => {
+    res.render('client', {
+        pets
+    })
+})
+
 
 router.get('/pet/:id', async (req, res) => {
     return res.render('pet', pets [req.params.id-1])
