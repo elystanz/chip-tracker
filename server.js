54ca13252fd34d2a679acc358a3dd846617cd9e7
yuval7994 <yuvalba45@gmail.com>
2022-05-26 19:51:53 -0500

    made nav work

--- server.js
+++ server.js
@@ -1,45 +1,50 @@
 const express = require("express")
 const path = require("path")
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')
-const sess = {
-  secret: 'dream team',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-
-}
-
-app.use(session(sess))
-
-// set up variable for handlebars module
+const session = require("express-session")
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const exphbs = require("express-handlebars")
 
 const routes = require("./controllers")
 const sequelize = require("./config/connection.js")
 
+const sess = {
+  secret: "dream team",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+}
+
+
+// set up variable for handlebars module
+
 const app = express()
+app.use(session(sess))
+
 const PORT = process.env.PORT || 3001
 
 // accept handlebars
-const hbs = exphbs.create({extname: 'handlebars', defaultLayout: 'main', layoutsDir: __dirname + '/views/layout'});
+const hbs = exphbs.create({
+  extname: "handlebars",
+  defaultLayout: "main",
+  layoutsDir: __dirname + "/views/layout",
+})
 
-app.engine("handlebars", hbs.engine);
-app.set('views', path.join(__dirname, 'views'));
+app.engine("handlebars", hbs.engine)
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "handlebars")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(require("./controllers/pets-routes"));
-app.use(require("./controllers/logger"));
+app.use(express.static(path.join(__dirname, "/public")))
+app.use(require("./controllers/pets-routes"))
+app.use(require("./controllers/logger"))
 
-app.get('/', (req, res) => {
-  res.render('main');
-});
+app.get("/", (req, res) => {
+  res.render("main")
+})
 
 // turn on routes
 app.use(routes)
