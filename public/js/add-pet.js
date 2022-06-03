45fd52759ca99a438c97917f657d65f49b915765
HoneyTwix <chocolateenchanted@gmail.com>
2022-06-02 19:13:16 -0500

    connect to jawsdb

--- public/js/add-pet.js
+++ public/js/add-pet.js
@@ -1,17 +1,28 @@
-const req = require("express/lib/request");
+// const req = require("express/lib/request");
 
 const addPetHandler = async (event) => {
     event.preventDefault
     const petName = document.querySelector('#petName').value.trim()
     const ownerName = document.querySelector('#ownerName').value.trim()
+    const chipped = document.form[0][1].value
+    console.log(chipped)
+    // let i;
+    // for(i=0;i<chipped.length;i++){
+    // if(chipped[i].checked){
+    //     chipped = chipped[i].value;
+    //     return chipped
+    //     }
+    // }
+    // console.log(petName)
+    // console.log(ownerName)
     // var chipper = ''
-    const chipped = document.querySelector('#chipped').checked
-    if (chipped) {
-        chipped = document.querySelector('#chipped').value
-    }
-     else{
-         chipped = document.querySelector('#notChipped').value
-     }
+    // const chipped = document.querySelector('#chipped').checked
+    // if (chipped) {
+    //     chipped = document.querySelector('#chipped').value
+    // }
+    //  else{
+    //      chipped = document.querySelector('#notChipped').value
+    //  }
     if (petName && ownerName && chipped){
         const response = await fetch('/api/vets/add-pet',{
             method: 'POST',
